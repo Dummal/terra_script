64f065b73dbe875a32d3be19262891d55a2883b2
Dummal <soundari1129@gmail.com>
2025-01-28 16:55:12 +0530

    Add Terraform configuration files

---
+++ main.tf
@@ -0,0 +1,78 @@
+resource "aws_organizations_organization" "org" {
+  feature_set = "ALL"
+}
+
+resource "aws_organizations_organizational_unit" "ou" {
+  for_each = toset(var.organizational_units)
+
+  name      = each.value
+  parent_id = aws_organizations_organization.org.roots[0].id
+
+  tags = {
+    Environment = "Production"
+    ManagedBy   = "Terraform"
+  }
+
+resource "aws_iam_role" "aft_execution_role" {
+  name               = "aft-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+
+  tags = {
+    Purpose    = "AFT"
+    ManagedBy  = "Terraform"
+  }
+
+resource "aws_iam_role" "aft_account_provisioning_role" {
+  name               = "aft-account-provisioning-role"
+  assume_role_policy = data.aws_iam_policy_document.org_assume_role.json
+
+  tags = {
+    Purpose    = "AFT"
+    ManagedBy  = "Terraform"
+  }
+
+resource "aws_iam_role" "aft_admin_role" {
+  name               = "aft-admin-role"
+  assume_role_policy = data.aws_iam_policy_document.admin_assume_role.json
+
+  tags = {
+    Purpose    = "AFT"
+    ManagedBy  = "Terraform"
+  }
+
+resource "aws_s3_bucket" "aft_logs" {
+  bucket = var.aft_logs_bucket_name
+
+  versioning {
+    enabled = true
+  }
+
+resource "aws_kms_key" "aft_key" {
+  description             = "KMS key for AFT resources"
+  enable_key_rotation     = true
+  deletion_window_in_days = 30
+
+  tags = {
+    Environment = "Production"
+    ManagedBy   = "Terraform"
+  }
+
+resource "aws_sns_topic" "aft_notifications" {
+  name = "aft-notifications"
+
+  kms_master_key_id = aws_kms_key.aft_key.arn
+
+  tags = {
+    Environment = "Production"
+    ManagedBy   = "Terraform"
+  }
+
+resource "aws_dynamodb_table" "aft_requests" {
+  name           = "aft-requests"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
--- variables.tf
+++ variables.tf
@@ -1,34 +1,26 @@
 variable "aws_region" {
-  description = "The AWS region to deploy resources in"
+  description = "The AWS region to deploy resources in."
   type        = string
   default     = "us-west-2"
 }
 
 variable "master_account_email" {
-  description = "Email address for the master account"
+  description = "Email address for the master account."
   type        = string
 }
 
 variable "master_account_id" {
-  description = "AWS Account ID for the master account"
+  description = "AWS Account ID for the master account."
   type        = string
 }
 
 variable "organizational_units" {
-  description = "List of Organizational Units to create"
+  description = "List of Organizational Units (OUs) to create."
   type        = list(string)
   default     = ["Security", "Audit Log"]
 }
 
 variable "aft_logs_bucket_name" {
-  description = "Name of the S3 bucket for AFT logs"
+  description = "Name of the S3 bucket for AFT logs."
   type        = string
 }
-
-variable "tags" {
-  description = "Tags to apply to all resources"
-  type        = map(string)
-  default = {
-    Environment = "Production"
-    ManagedBy   = "Terraform"
-  }
