dd730add67591b941dcf97953486eb7b56e499ab
Dummal <soundari1129@gmail.com>
2025-01-28 18:07:11 +0530

    Add Terraform configuration files

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "organization_id" {
   value       = module.control_tower.organization_id
 }
 
+output "organization_root_id" {
+  description = "The root ID of the AWS Organization"
+  value       = module.control_tower.organization_root_id
+}
+
 output "security_ou_id" {
   description = "The ID of the Security Organizational Unit"
   value       = module.control_tower.security_ou_id
@@ -46,44 +51,4 @@ output "sns_topic_arn" {
 output "dynamodb_table_name" {
   description = "The name of the DynamoDB table"
   value       = module.aws_resources.dynamodb_table_name
-}
-
-output "organization_id" {
-  value = aws_organizations_organization.org.id
-}
-
-output "security_ou_id" {
-  value = aws_organizations_organizational_unit.ou["Security"].id
-}
-
-output "audit_log_ou_id" {
-  value = aws_organizations_organizational_unit.ou["Audit Log"].id
-}
-
-output "aft_execution_role_arn" {
-  value = aws_iam_role.aft_execution_role.arn
-}
-
-output "aft_account_provisioning_role_arn" {
-  value = aws_iam_role.aft_account_provisioning_role.arn
-}
-
-output "aft_admin_role_arn" {
-  value = aws_iam_role.aft_admin_role.arn
-}
-
-output "s3_bucket_id" {
-  value = aws_s3_bucket.aft_logs.id
-}
-
-output "kms_key_arn" {
-  value = aws_kms_key.aft_key.arn
-}
-
-output "sns_topic_arn" {
-  value = aws_sns_topic.aft_notifications.arn
-}
-
-output "dynamodb_table_name" {
-  value = aws_dynamodb_table.aft_requests.name
 }
