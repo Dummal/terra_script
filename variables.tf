d0535ff74cb8631ffdba88ffdb0de3dfe4837b19
Dummal <soundari1129@gmail.com>
2025-01-28 17:41:56 +0530

    Add Terraform configuration files

--- variables.tf
+++ variables.tf
@@ -24,11 +24,3 @@ variable "aft_logs_bucket_name" {
   description = "Name of the S3 bucket for AFT logs"
   type        = string
 }
-
-variable "tags" {
-  description = "Tags to apply to all resources"
-  type        = map(string)
-  default = {
-    Environment = "Production"
-    ManagedBy   = "Terraform"
-  }
